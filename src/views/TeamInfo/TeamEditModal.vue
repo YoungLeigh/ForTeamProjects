c8a1a9f96e12f6dc757d4fcc2f25d4a6f70e0fe5
YoungLeigh <yoonyoung.lee1218@gmail.com>
2023-03-15 01:26:14 +0900

    Update TeamEditModal.vue

--- src/views/TeamInfo/TeamEditModal.vue
+++ src/views/TeamInfo/TeamEditModal.vue
@@ -1,67 +1,8 @@
 <template>
   <div class="teamEditModal">
-    <table class="teamAddModal-table">
-      <thead>
-        <th class="table-name team-add-th">+ Add a Member</th>
-
-        <th class="table-email team-add-th"></th>
-
-        <th class="table-contact team-add-th"></th>
-
-        <th class="table-actions team-add-th"></th>
-      </thead>
-      <tbody>
-        <tr class="team-info-tr">
-          <td class="table-name team-info-td">
-            <input
-              @keyup.enter.prevent="addNewMember"
-              @keyup.escape.prevent="closeAddBtn"
-              class="team-add-input"
-              type="text"
-              name="newName"
-              placeholder="Name"
-              v-model="memberName"
-            />
-          </td>
-
-          <td class="table-email team-info-td">
-            <input
-              @keyup.enter.prevent="addNewMember"
-              @keyup.escape.prevent="closeAddBtn"
-              class="team-add-input"
-              type="email"
-              name="newEmail"
-              placeholder="example@gmail.com"
-              v-model="memberEmail"
-            />
-          </td>
-
-          <td class="table-contact team-info-td">
-            <input
-              @keyup.enter.prevent="addNewMember"
-              @keyup.escape.prevent="closeAddBtn"
-              class="team-add-input"
-              type="text"
-              name="newContacts"
-              placeholder="000-000-000"
-              v-model="memberContacts"
-            />
-          </td>
-
-          <td class="td-btn team-info-td">
-            <button
-              @click="addNewMember"
-              class="team-close-btn1 btn--full2 edit-btn"
-            >
-              <font-awesome-icon class="team-icon3" icon="fa-solid fa-check" />
-            </button>
-            <button @click="closeAddBtn" class="team-close-btn">
-              <font-awesome-icon class="team-icon4" icon="fa-solid fa-xmark" />
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="teamEdit-container">
+      <h3>Edit information</h3>
+    </div>
   </div>
 </template>
 
@@ -81,11 +22,19 @@ export default {
 <style>
 .teamEditModal {
   position: absolute;
+  display: inline-block;
   top: 50%;
   left: 0;
   right: 0;
-  height: 450px;
+  background-color: #424954;
+  width: 360px;
+  right: 155px;
+  top: 0;
+  left: 0;
+}
+.teamEdit-container {
   display: flex;
   flex-direction: column;
+  color: #fff;
 }
 </style>
