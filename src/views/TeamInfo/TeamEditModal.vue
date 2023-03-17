b1f0c77957a58e35f03c4e359b5e53229f0d4e54
YoungLeigh <yoonyoung.lee1218@gmail.com>
2023-03-17 18:36:45 +0900

    Update TeamEditModal.vue

--- src/views/TeamInfo/TeamEditModal.vue
+++ src/views/TeamInfo/TeamEditModal.vue
@@ -2,6 +2,24 @@
   <div class="teamEditModal">
     <div class="teamEdit-container">
       <h3>Edit information</h3>
+      <div class="modal-content">
+        <form>
+          <div class="form-group">
+            <label for="name-input">Name:</label>
+            <input type="text" id="name-input" name="name" />
+          </div>
+          <div class="form-group">
+            <label for="email-input">Email:</label>
+            <input type="email" id="email-input" name="email" />
+          </div>
+          <div class="form-group">
+            <label for="phone-input">Contacts:</label>
+            <input type="tel" id="phone-input" name="phone" />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        <button id="close-modal">Close Modal</button>
+      </div>
     </div>
   </div>
 </template>
@@ -24,17 +42,43 @@ export default {
   position: absolute;
   display: inline-block;
   top: 50%;
-  left: 0;
-  right: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
   background-color: #424954;
   width: 360px;
-  right: 155px;
-  top: 0;
-  left: 0;
 }
 .teamEdit-container {
   display: flex;
   flex-direction: column;
-  color: #fff;
+  color: #383838;
+}
+.modal-container {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: white;
+  margin: 10% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  max-width: 600px;
+}
+
+.form-group {
+  margin-bottom: 1rem;
+}
+
+label {
+  display: block;
+  margin-bottom: 0;
 }
 </style>
