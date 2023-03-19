65f2fa629cbfc78c09ab4ee564f4479e023d1bfe
YoungLeigh <yoonyoung.lee1218@gmail.com>
2023-03-19 14:21:50 +0900

    teamEditModal css fin

--- src/views/TeamInfo/TeamEditModal.vue
+++ src/views/TeamInfo/TeamEditModal.vue
@@ -2,20 +2,21 @@
   <div class="teamEditModal">
     <div class="teamEdit-container">
       <h3 class="teamEdit-header">Edit information</h3>
+      <hr class="teamEdit-underline" />
       <div class="teamEdit-modal-content">
         <div class="teamEdit-input">
           <label class="teamEdit-label">Name:</label>
-          <input type="text" class="teamEdit-input" />
+          <input type="text" class="teamEdit-input-field" />
         </div>
         <div class="teamEdit-input">
           <label class="teamEdit-label">Email:</label>
-          <input type="email" class="teamEdit-input" />
+          <input type="email" class="teamEdit-input-field" />
         </div>
         <div class="teamEdit-input">
           <label class="teamEdit-label">Contacts:</label>
-          <input type="tel" class="teamEdit-input" />
+          <input type="tel" class="teamEdit-input-field" />
         </div>
-        <button class="edit-submit-btn" type="submit">Submit</button>
+        <button class="edit-submit-btn" type="submit">Save</button>
         <button id="teamEdit-close-btn">
           <font-awesome-icon icon="fa-solid fa-xmark" />
         </button>
@@ -49,11 +50,21 @@ export default {
   border-radius: 15px;
 }
 .teamEdit-header {
-  font-size: 25px;
+  font-size: 20px;
   text-decoration: none;
   color: #424954;
   white-space: nowrap;
   margin-top: 15px;
+  margin-bottom: 0px;
+}
+.teamEdit-underline {
+  border: 0;
+  clear: both;
+  display: block;
+  width: 50%;
+  background-color: #4f4f4fc4;
+  height: 1px;
+  margin-left: 0;
 }
 .teamEdit-container {
   display: flex;
@@ -80,29 +91,31 @@ export default {
 .form-group {
   margin-bottom: 1rem;
 }
+.teamEdit-input-field {
+  width: 90%;
+  padding-left: 5px;
+  height: 30px;
+  border-radius: 5px;
+  border-style: none;
+  font-family: mainFont, korFont;
+  font-size: 16px;
+  background-color: none;
+  border: 2px solid #424954;
+}
 .edit-submit-btn {
   border-style: none;
-  height: 30px;
+  height: 35px;
   background-color: #424954;
   border-radius: 5px;
   font-family: mainFont, korFont;
-  font-size: 18px;
+  font-size: 16px;
   color: #fff;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   cursor: pointer;
-}
-.teamEdit-input {
-  width: 90%;
-  padding-left: 5px;
-  width: 340px;
-  height: 50px;
-  margin-bottom: 25px;
-  border-radius: 5px;
-  background-color: #ebebeb;
-  border-style: none;
-  color: #818181;
-  font-family: mainFont, korFont;
-  font-size: 19px;
+  margin-top: 5px;
+  margin-left: 74%;
+  padding-left: 15px;
+  padding-right: 15px;
 }
 #teamEdit-close-btn {
   background-color: transparent;
@@ -116,6 +129,7 @@ export default {
   position: absolute;
   top: 10px;
   right: 10px;
+  color: #424954;
 }
 #teamEdit-close-btn:hover,
 #teamEdit-close-btn:active {
@@ -124,6 +138,7 @@ export default {
 
 .teamEdit-label {
   display: block;
-  margin-bottom: 5px;
+  margin-bottom: 2px;
+  color: #424954;
 }
 </style>
